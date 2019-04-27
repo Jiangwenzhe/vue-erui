855b95007ef349eeea1251ed6f1bc87d98c3d820
Erzhuyijian <1821344102@qq.com>
2019-04-27 23:13:27 +0800

    添加初始button组件

--- src/button.vue
+++ src/button.vue
@@ -1,5 +1,5 @@
 <template>
-
+  <button class="g-button">按钮 </button>
 </template>
 
 <script>
@@ -8,6 +8,22 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+  .g-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+    &:focus {
+      outline: none;
+    }
+  }
 </style>
