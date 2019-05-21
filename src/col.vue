c8f3bfd969e01804c08a2ac8ffea392e20ef9277
Erzhuyijian <1821344102@qq.com>
2019-05-21 21:55:48 +0800

    基本实现网格系统的功能

--- src/col.vue
+++ src/col.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
+  <div class="col"
+    :class="[`col-${span}`, offset && `offset-${offset}`]"
+    :style="{paddingLeft: gap/2 +'px', paddingRight: gap/2+ 'px'}"
+  >
     <slot></slot>
   </div>
 </template>
@@ -10,6 +13,14 @@ export default {
   props: {
     span: {
       type: [Number, String]
+    },
+    offset: {
+      type: [Number, String]
+    },
+  },
+  data() {
+    return {
+      gap: 0
     }
   }
 }
@@ -18,15 +29,20 @@ export default {
 <style lang="scss" scoped>
   .col {
     height: 100px;
-    background: grey;
     width: 50%;
     border: 1px solid red;
 
     $class-prefix: col-;
-
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         width: ($n / 24) * 100%
+      }
+    }
+
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%
       }
     }
   }
--- src/row.vue
+++ src/row.vue
@@ -1,17 +1,28 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft: -gap/2+'px', marginRight: -gap/2+'px'}">
     <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'er-row',
+  props: {
+    gap: {
+      type:[Number, String]
+    }
+  },
+  mounted() {
+    this.$children.forEach(vm => {
+      vm.gap = this.gap
+    })
+  }
 }
 </script>
 
 <style lalng="scss" scoped>
   .row {
     display: flex;
+    margin: 0 -10px;
   }
 </style>
