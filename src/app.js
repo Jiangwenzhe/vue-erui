bdf886b76ea7cf8122dd2e8cc0a852b19c3f2431
Erzhuyijian <1821344102@qq.com>
2019-05-21 19:47:41 +0800

    完成网格系统的雏形

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group.vue'
 import Input from './input.vue'
+import Row from './row.vue'
+import Col from './col.vue'
 
 Vue.component('er-button', Button)
 Vue.component('er-icon', Icon)
 Vue.component('er-button-group', ButtonGroup)
 Vue.component('er-input', Input)
+Vue.component('er-row', Row)
+Vue.component('er-col', Col)
 
 new Vue({
   el: '#app',
